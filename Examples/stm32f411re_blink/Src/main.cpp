a5b5ba64b286c2256f20c45488e24e281644540d
teterevlev <o.damage@gmail.com>
2021-12-07 18:36:11 +0300

    Separated files

--- Examples/stm32f411re_blink/Src/main.cpp
+++ Examples/stm32f411re_blink/Src/main.cpp
@@ -1,5 +1,6 @@
 /* The reset and clock control module */
 #include <stm32f411xe.h>
+#include "tPeriphery.h"
 
 uint32_t clocksPerSecond = 7200000;
 uint32_t clocksPerMillie = clocksPerSecond/1000;
@@ -9,57 +10,7 @@ void delay (uint32_t millis){
 	while ( count-- );
 }
 
-enum{INPUT, OUTPUT, AF, ANALOG};
-enum{PUSHPULL, OPENDRAIN};
-enum{LOW, MEDIUM, FAST, VERYFAST};
-enum {OFF, ON};
-enum {PULLUP=1, PULLDOWN};
-class GPIO_Port{
-	private:
-		GPIO_TypeDef * port;
-		uint32_t AHB1ENR_bit;
-	public:
-		GPIO_Port(GPIO_TypeDef * port, uint32_t AHB1ENR_bit):
-			port(port),
-			AHB1ENR_bit(AHB1ENR_bit)
-		{
-
-		}
-		void clock(){
-			RCC->AHB1ENR |= AHB1ENR_bit;
-		}
-		void clock(uint8_t off){
-			if (off){
-				RCC->AHB1ENR |= AHB1ENR_bit;
-			}else{
-				RCC->AHB1ENR &= ~AHB1ENR_bit;
-			}
-		}
-		void output(uint8_t pin, uint8_t mode = OUTPUT, uint8_t type = PUSHPULL, uint8_t speed = VERYFAST){
-			uint8_t		pin_x2		=	pin * 2;
-			uint32_t	mask		=	1 << pin;
-			uint32_t	mask_x2		=	0b11 << pin_x2;
-
-			port->MODER				&=	mask_x2;				// clear mode
-			port->MODER				|=	mode << pin_x2;			// mode
-
-			port->OTYPER			&=	mask;					// clear type
-			port->OTYPER			|=	type << pin;			// type
-
-			port->OSPEEDR			&=	mask_x2;				// clear speed
-			port->OSPEEDR			|=	speed << pin_x2;		// speed
-		}
-		void input(uint8_t pin, uint8_t pullup = OUTPUT){
-			uint8_t		pin_x2		=	pin * 2;
-			uint32_t	mask		=	1 << pin;
-			uint32_t	mask_x2		=	0b11 << pin_x2;
-
-			port->MODER			&=	mask;					// mode = input
-
-			port->PUPDR			&=	mask;					// pullup
-			port->PUPDR			|=	mask_x2 << pin_x2;		// pullup
-		}
-} PortA(GPIOA, RCC_AHB1ENR_GPIOAEN), PortB(GPIOB, RCC_AHB1ENR_GPIOBEN);
+GPIO_Port PortA(GPIOA, RCC_AHB1ENR_GPIOAEN), PortB(GPIOB, RCC_AHB1ENR_GPIOBEN);
 
 int main ( void ){
 
@@ -80,7 +31,7 @@ int main ( void ){
 
 	for ( ;; ) {
 		GPIOA->ODR ^= GPIO_ODR_OD5;
-		delay(100);
+		delay(500);
 	}
 }
 
---
+++ Examples/stm32f411re_blink/Src/tPeriphery.cpp
@@ -0,0 +1,9 @@
+/*
+ * tPeriphery.cpp
+ *
+ *  Created on: 7 дек. 2021 г.
+ *      Author: odama
+ */
+
+#include "tPeriphery.h"
+
