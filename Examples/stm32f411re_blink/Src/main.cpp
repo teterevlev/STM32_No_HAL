fcecdeacddde49e12ed6766d373f64f4df93dc1e
teterevlev <o.damage@gmail.com>
2021-12-07 18:27:37 +0300

    GPIO config class

--- Examples/stm32f411re_blink/Src/main.cpp
+++ Examples/stm32f411re_blink/Src/main.cpp
@@ -1,5 +1,6 @@
 /* The reset and clock control module */
 #include <stm32f411xe.h>
+
 uint32_t clocksPerSecond = 7200000;
 uint32_t clocksPerMillie = clocksPerSecond/1000;
 void delay (uint32_t millis){
@@ -7,6 +8,59 @@ void delay (uint32_t millis){
 
 	while ( count-- );
 }
+
+enum{INPUT, OUTPUT, AF, ANALOG};
+enum{PUSHPULL, OPENDRAIN};
+enum{LOW, MEDIUM, FAST, VERYFAST};
+enum {OFF, ON};
+enum {PULLUP=1, PULLDOWN};
+class GPIO_Port{
+	private:
+		GPIO_TypeDef * port;
+		uint32_t AHB1ENR_bit;
+	public:
+		GPIO_Port(GPIO_TypeDef * port, uint32_t AHB1ENR_bit):
+			port(port),
+			AHB1ENR_bit(AHB1ENR_bit)
+		{
+
+		}
+		void clock(){
+			RCC->AHB1ENR |= AHB1ENR_bit;
+		}
+		void clock(uint8_t off){
+			if (off){
+				RCC->AHB1ENR |= AHB1ENR_bit;
+			}else{
+				RCC->AHB1ENR &= ~AHB1ENR_bit;
+			}
+		}
+		void output(uint8_t pin, uint8_t mode = OUTPUT, uint8_t type = PUSHPULL, uint8_t speed = VERYFAST){
+			uint8_t		pin_x2		=	pin * 2;
+			uint32_t	mask		=	1 << pin;
+			uint32_t	mask_x2		=	0b11 << pin_x2;
+
+			port->MODER				&=	mask_x2;				// clear mode
+			port->MODER				|=	mode << pin_x2;			// mode
+
+			port->OTYPER			&=	mask;					// clear type
+			port->OTYPER			|=	type << pin;			// type
+
+			port->OSPEEDR			&=	mask_x2;				// clear speed
+			port->OSPEEDR			|=	speed << pin_x2;		// speed
+		}
+		void input(uint8_t pin, uint8_t pullup = OUTPUT){
+			uint8_t		pin_x2		=	pin * 2;
+			uint32_t	mask		=	1 << pin;
+			uint32_t	mask_x2		=	0b11 << pin_x2;
+
+			port->MODER			&=	mask;					// mode = input
+
+			port->PUPDR			&=	mask;					// pullup
+			port->PUPDR			|=	mask_x2 << pin_x2;		// pullup
+		}
+} PortA(GPIOA, RCC_AHB1ENR_GPIOAEN), PortB(GPIOB, RCC_AHB1ENR_GPIOBEN);
+
 int main ( void ){
 
 
@@ -19,11 +73,9 @@ int main ( void ){
 
 
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	PortA.clock();
+	PortA.output(5);
 
-
-	GPIOA->MODER |= 1 << GPIO_MODER_MODER5_Pos;
-	GPIOA->OTYPER |= 0 << GPIO_OTYPER_OT5_Pos;
-	GPIOA->OSPEEDR |= 3 << GPIO_OSPEEDR_OSPEED5_Pos;
 
 
 	for ( ;; ) {
